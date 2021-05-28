651ef1838fbe8b86870c4350db5d76670d99267d
rishthaz <rishthask@gmail.com>
2021-05-29 00:26:26 +0400

    wsgi changes

--- ProdTracker/ProdTracker/wsgi.py
+++ ProdTracker/ProdTracker/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
+
+# Add project directory to the sys.path
+path_home = str(Path(__file__).parents[1])
+if path_home not in sys.path:
+    sys.path.append(path_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProdTracker.settings')
 
