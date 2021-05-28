027ac1a7d5d54a157f9a8dcf5cb6116a76bf7c7c
rishthaz <rishthask@gmail.com>
2021-05-28 22:20:34 +0400

    AlphaVIsion DB settings changes

--- ProdTracker/ProdTracker/dev_settings.py
+++ ProdTracker/ProdTracker/dev_settings.py
@@ -23,7 +23,7 @@ from .base_settings import *
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+       'NAME': BASE_DIR / 'db_alpha.sqlite3',
    }
 }
 
