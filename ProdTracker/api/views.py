d318a82249a3ce3c6768fb79feceab780c74cc2d
rishthaz <rishthask@gmail.com>
2021-05-31 09:34:14 +0400

    len sale

--- ProdTracker/api/views.py
+++ ProdTracker/api/views.py
@@ -420,7 +420,7 @@ class ProductViewSet(viewsets.ModelViewSet):
         sale_len = sum([1 for x in sale if x > 0])
         if sale_len == 0:
             sale_len = 1
-        response["average"] = [sum(sale) / sale_len]* sale_len
+        response["average"] = [sum(sale) / sale_len]* len(sale)
         return Response(response)
 
 class ProductAggViewSet(viewsets.ModelViewSet):
